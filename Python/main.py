320782f83c5c2e61c234a233ec307da4645098fb
ArturHaavisto <arturhaavisto1@gmail.com>
2023-01-24 17:25:12 +0200

    working solution for every situation

--- Python/main.py
+++ Python/main.py
@@ -7,13 +7,16 @@ from lib.math import normalize_heading
 import time
 import random
 import math
+import threading as th
 
 FRONTEND_BASE = "noflight.monad.fi"
 BACKEND_BASE = "noflight.monad.fi/backend"
 
 game_id = None
 
-route = []
+route = [-1]
+
+still_time = True
 
 def on_message(ws: websocket.WebSocketApp, message):
     [action, payload] = json.loads(message)
@@ -45,7 +48,7 @@ def on_close(ws, close_status_code, close_msg):
 def generate_commands(game_state):
     global route
     
-    if(len(route) == 0):
+    if(route[0] == -1):
         print(game_state)
         generate_route(game_state)
     
@@ -71,42 +74,64 @@ def generate_commands(game_state):
 
 def generate_route(game_state):
     global route
+    global still_time
 
-    total_limit = 100000
-    route_steps_limit = 60
+    s = th.Timer(25.0, no_time_left)  
+    s.start()
+
+    total_limit = 1000000
+    route_steps_limit = 100
     direction_change_limit = 20
-    found_routes_limit = 1000
-    best_routes_min = 20
+    found_routes_limit = 10000
     best_routes_max_direction_difference = 20
 
     # [x_min, y_min, x_max, y_max]
     boundaries = get_boundaries(game_state["bbox"])
 
     # [x, y, direction, speed, collision_radius, id, target_x, target_y, target_direction, landing_radius]
-    aircrafts = get_aircrafts_data(game_state)
+    aircrafts_all = get_aircrafts_data(game_state)
+    aircrafts = []
+    more_aircrafts = True
 
     # [[score, [airplane1 directions], [airplane2 directions], ...], ...]
     found_routes = []
-    
-    most_steps = 0
+
+    saved_routes = []
+    saved_blacklist = []
+    last_resort_success = False
+    counter = 0
+
+    last_resort = True
 
     c1 = 1
     c2 = 1
-    while c1 < total_limit and len(found_routes) <= found_routes_limit:
+    while c1 < total_limit and len(found_routes) <= found_routes_limit and still_time:
         if c2 >= 10000:
-            print('Random routes: ' + str(c1) + ', Routes count: ' + str(len(found_routes)) + 
-                ', Most steps: ' + str(most_steps))
+            print('Random routes: ' + str(c1) + ', Routes count: ' + str(len(found_routes)))
+            if len(found_routes) > 0:
+                found_routes.sort(key=lambda x: x[0])
+                print("best score: " + str(found_routes[0][0]))
             c2 = 1
         else:
             c2 += 1
 
         # [score, [airplane1 directions], [airplane2 directions], ...]
-        current_routes = []
-        for i in range(0, len(aircrafts)):
-            current_routes.append([])
+        current_routes = saved_routes.copy()
         
-        blacklist = []
+        blacklist = saved_blacklist.copy()
         success = False
+
+        if last_resort:
+            if(len(aircrafts) < len(aircrafts_all) and more_aircrafts):
+                aircrafts = aircrafts_all[:(len(blacklist) + 1)].copy()
+                current_routes.append([])
+                more_aircrafts = False
+            else:
+                for i in range(len(blacklist), len(aircrafts)):
+                    current_routes.append([])        
+        else:
+            for i in range(0, len(aircrafts)):
+                current_routes.append([])
 
         stop = False
         i = 0
@@ -115,8 +140,8 @@ def generate_route(game_state):
                 if j in blacklist:
                     continue
                 aircraft = aircrafts[j]
-                if(len(current_routes[j]) > 0 and calculate_distance(current_routes[j][-1][0], current_routes[j][-1][1], aircraft[6], aircraft[7]) <= aircraft[9] and
-                    compare_directions(current_routes[j][-1][2], aircraft[8], best_routes_max_direction_difference)):
+                if(len(current_routes[j]) > 0 and calculate_distance(current_routes[j][-1][0], current_routes[j][-1][1], aircraft[6], aircraft[7]) < aircraft[9] and 
+                compare_directions(current_routes[j][-1][2], aircraft[8], best_routes_max_direction_difference)):
                     blacklist.append(j)
                     continue
                 start_x = 0
@@ -133,54 +158,84 @@ def generate_route(game_state):
                 new_direction = get_rand_direction(start_direction - direction_change_limit, start_direction + direction_change_limit)
                 new_x = calculate_x(start_x, new_direction, aircraft[3])
                 new_y = calculate_y(start_y, new_direction, aircraft[3])
-                if new_x < boundaries[0] or new_x > boundaries[2] or new_y < boundaries[1] or new_y > boundaries[3]:
+                if new_x <= boundaries[0] or new_x >= boundaries[2] or new_y <= boundaries[1] or new_y >= boundaries[3]:
                     stop = True
                     break
-                if(j > 0 and check_collision(new_x, new_y, current_routes, aircrafts, j, blacklist)):
+                if(j > 0 and check_collision(new_x, new_y, current_routes, aircrafts, j, i)):
                     stop = True
                     break
                 current_routes[j].append([new_x, new_y, new_direction])
             i += 1
             if(len(blacklist) == len(aircrafts)):
+                if(len(blacklist) == len(aircrafts_all)):
+                    last_resort_success = True
                 success = True
                 break
         c1 += 1
+        if last_resort:
+            counter += 1
+            if counter >= 10000 and not last_resort_success:
+                counter = 0
+                aircrafts = []
+                saved_routes = []
+                saved_blacklist = []
+                more_aircrafts = True
+        
+        if last_resort and success:
+            direction = aircrafts[-1][8]
+            x = current_routes[-1][-1][0]
+            y = current_routes[-1][-1][1]
+            new_x = calculate_x(x, direction, aircrafts[-1][3])
+            new_y = calculate_y(y, direction, aircrafts[-1][3])
+            current_routes[-1].append([new_x, new_y, direction])            
+            if not last_resort_success:
+                saved_routes = current_routes.copy()
+                saved_blacklist = blacklist.copy()
+                more_aircrafts = True
+                continue
         if success:
-            score = i + 1
+            sizes = []
+            for i in range(0, len(aircrafts)):
+                sizes.append(len(current_routes[i]))
+            score = max(sizes)
             routes = [0]
             for i in range(0, len(aircrafts)):
-                routes.append([])
-                x = aircrafts[i][0]
-                y = aircrafts[i][1]
+                temp_route = []
                 direction = aircrafts[i][2]
                 for j in range(0, len(current_routes[i])):
-                    actual_direction = calculate_direction(x, y, current_routes[i][j][0], current_routes[i][j][1])
-                    direction_change = get_direction_change(direction, actual_direction)
-                    routes[i + 1].append(direction_change)
+                    direction_change = get_direction_change(direction, current_routes[i][j][2])
+                    temp_route.append(direction_change)
                     if(direction_change != 0):
                         score += 1
-                    x = current_routes[i][j][0]
-                    y = current_routes[i][j][1]
                     direction = current_routes[i][j][2]
                     if(j == len(current_routes[i]) - 1):
-                        direction_change = get_direction_change(direction, aircrafts[i][8])
-                        if(direction_change != 0):
-                            score += 1
-                        routes[i + 1].append(direction_change)
-                        routes[i + 1].append(aircrafts[i][5])
+                        temp_route.append(aircrafts[i][5])
+                routes.append(temp_route)
             routes[0] = score
             found_routes.append(routes)
+            found_routes.sort(key=lambda x: x[0])
+            saved_blacklist = []
+            saved_routes = []
+            aircrafts = []
+            more_aircrafts = True
+            last_resort_success = False
+            counter = 0
             continue
-        if(most_steps < i):
-            most_steps = i
-    
+        if(not last_resort and c1 > 20000 and len(found_routes) == 0):
+            last_resort = True
+            aircrafts = []
+    s.cancel()
+
     if(len(found_routes) == 0):
-        print("Most steps: " + str(most_steps))
-        return []
+        route = []
+        return
     found_routes.sort(key=lambda x: x[0])
+    scores = []
+    for i in range(0, len(found_routes)):
+        scores.append(found_routes[i][0])
+    print(scores)
 
     route = found_routes[0][1:]
-    print(route)
 
 
 def get_aircrafts_data(game_state):
@@ -203,8 +258,8 @@ def get_target_info(destination, speed, airports):
     target_info = []
     for airport in airports:
         if(airport["name"] == destination):
-            target_x = airport["position"]["x"] + -math.cos(airport["direction"]) * speed
-            target_y = airport["position"]["y"] + -math.sin(airport["direction"]) * speed
+            target_x = airport["position"]["x"] + -math.cos(math.radians(airport["direction"])) * speed
+            target_y = airport["position"]["y"] + -math.sin(math.radians(airport["direction"])) * speed
             target_info = [
                 target_x,
                 target_y,
@@ -228,12 +283,12 @@ def get_rand_direction(min, max):
         direction -= 360
     return direction
 
-def check_collision(x, y, current_routes, aircrafts, aircraft_index, blacklist):
+def check_collision(x, y, current_routes, aircrafts, aircraft_index, step):
     for i in range(0, aircraft_index):
-        if(i in blacklist):
+        if(len(current_routes[i]) - 1 < step):
             continue
-        if(calculate_distance(x, y, current_routes[i][-1][0], current_routes[i][-1][1]) <= max(aircrafts[aircraft_index][4], aircrafts[i][4])):
-                return True
+        if(calculate_distance(x, y, current_routes[i][step][0], current_routes[i][step][1]) <= max(aircrafts[aircraft_index][4], aircrafts[i][4])):
+            return True
     return False
 
 def calculate_distance(x1, y1, x2, y2):
@@ -261,6 +316,10 @@ def get_direction_change(current_direction, target_direction):
     if(direction_change < -180):
         direction_change += 360
     return direction_change
+
+def no_time_left():
+    global still_time
+    still_time = False
 
 def main():
     config = dotenv_values()
